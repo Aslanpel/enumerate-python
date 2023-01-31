e83e71e655d47a2bfdb80d83287ff3d3c05b1f26
Aslanpel <123958131+Aslanpel@users.noreply.github.com>
2023-01-31 09:40:38 +0100

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -1,4 +1,4 @@
-  lenguajes = ["Java", "C", "C++", "C#", "Python", "HTML", "Go", "Lua", "Swift", "Javascript", "Typescript", "Visual Basic", "Ruby", "Rust"]
+lenguajes = ["Java", "C", "C++", "C#", "Python", "HTML", "Go", "Lua", "Swift", "Javascript", "Typescript", "Visual Basic", "Ruby", "Rust"]
   
-  for indice, lenguaje in enumerate (lenguajes):
+for indice, lenguaje in enumerate (lenguajes):
     print (indice, lenguaje)
